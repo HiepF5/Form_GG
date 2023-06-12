b6169270a178941ca17949021ab8158590d92ca2
Nguyễn Công Hiệp <hiepf5.nguyencong.ptit@gmail.com>
2023-06-12 16:51:49 +0700

    f6

--- js/listform.js
+++ js/listform.js
@@ -1,3 +1,13 @@
 //call API
 const BASE_API = 'http://localhost:3000';
-fetch("")
+const API_GET_ALL_LIST = `${BASE_API}/lists`
+const resCallAPI = fetch(API_GET_ALL_LIST ,{
+    headers:{
+        "Content-Type":"application/json"
+    }
+
+})
+resCallAPI
+.then((Response)=>Response.json())
+.then((data)=>console.log(data))
+.catch((err)=> console.log(err));
